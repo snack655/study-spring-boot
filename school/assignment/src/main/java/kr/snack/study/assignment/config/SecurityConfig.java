cc6b46999377725958c6d2e50d51b593f55054f7
최민재 <cmjcmj1230@gmail.com>
2022-11-30 21:43:46 +0900

    back up....ㅠ

--- school/assignment/src/main/java/kr/snack/study/assignment/config/SecurityConfig.java
+++ school/assignment/src/main/java/kr/snack/study/assignment/config/SecurityConfig.java
@@ -28,15 +28,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
         http.authorizeRequests()
                 .antMatchers("/css/**", "/js/**", "/fonts/**", "/images/**").permitAll()
                 .antMatchers(HttpMethod.GET, "/list").permitAll()
                 .antMatchers("/").permitAll()
                 .antMatchers(HttpMethod.GET, "/view/**").permitAll()
-                .antMatchers("/user/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/user/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/user/**").permitAll()
                 .anyRequest().authenticated();
-
-        http.csrf().disable();
 
         http.formLogin()
                 .loginPage("/user/login")
--- school/assignment/src/main/java/kr/snack/study/assignment/controller/MemberController.java
+++ school/assignment/src/main/java/kr/snack/study/assignment/controller/MemberController.java
@@ -2,6 +2,7 @@ package kr.snack.study.assignment.controller;
 
 import kr.snack.study.assignment.controller.form.LoginForm;
 import kr.snack.study.assignment.controller.form.RegisterForm;
+import kr.snack.study.assignment.controller.util.ScriptUtils;
 import kr.snack.study.assignment.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -12,8 +13,10 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.io.IOException;
 
 @Controller
 @RequiredArgsConstructor
@@ -30,8 +33,15 @@ public class MemberController {
     @PostMapping("/user/login")
     public String login(
             @ModelAttribute("form") @Valid LoginForm form,
-            HttpServletResponse response
-    ) {
+            HttpServletResponse response,
+            BindingResult bindingResult
+    ) throws IOException {
+        System.out.println("----------------------- 여기 들어왔는지만 좀 봐야겠노..");
+        if (bindingResult.hasErrors()) {
+            System.out.println("로그인에서 걸렸습니다.... 제발 제발 걸렸닥 해줘");
+            ScriptUtils.alertAndMovePage(response, bindingResult.toString(), "/members/login");
+        }
+
         String token = memberService.login(form);
         Cookie cookie = new Cookie("token", token);
         cookie.setPath("/");
@@ -47,7 +57,16 @@ public class MemberController {
     }
 
     @PostMapping("/user/register")
-    public String register(@ModelAttribute("form") @Valid RegisterForm form, BindingResult bindingResult) {
+    public String register(
+            @ModelAttribute("form") @Valid RegisterForm form,
+            BindingResult bindingResult,
+            HttpServletResponse response
+    ) throws IOException {
+
+        if (bindingResult.hasErrors()) {
+            ScriptUtils.alert(response, bindingResult.toString());
+            return "members/register";
+        }
         memberService.register(form);
         return "redirect:/user/login";
     }
